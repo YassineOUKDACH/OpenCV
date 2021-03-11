bb3135d3dceedbf7d528a5b641371b751afbc447
YasssineOUKDACH <59118937+YassineOUKDACH@users.noreply.github.com>
2021-03-11 12:10:35 +0100

    Delete pnads.py

--- pnads.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-print("blblablablaba")
-x = np.array([[1,2,3],
-             [4,5,6]])
-st = StandardScaler()
-st.fit_transform(x)
-print(st)
